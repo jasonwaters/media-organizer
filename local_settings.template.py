592ae59b4f45c31f71393148a90e8d39f6034cd6
Jason Waters <jason@myheck.net>
2014-12-27 22:24:18 -0700

    local settings

---
+++ local_settings.template.py
@@ -0,0 +1,9 @@
+DOWNLOAD_FOLDER = "/path/to/downloads"
+TV_FOLDER = "/path/to/tvshows"
+MOVIE_FOLDER = "/path/to/movies"
+TRASH_FOLDER = "/path/to/trashcan"
+
+TRANSMISSION_HOST = "localhost"
+TRANSMISSION_PORT = 9091
+TRANSMISSION_USER = ""
+TRANSMISSION_PASSWORD = ""
